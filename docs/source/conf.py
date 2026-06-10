32ba5f9182bc35e8f7b343bcf4cd7a5d6481feff
Leonard <myfavoriteornot@gmail.com>
2026-06-11 00:45:40 +0200

    change

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 project = 'SoDiferent'
 copyright = '2026, Leonard Lorenc'
 author = 'Leonard Lorenc'
-release = '0.0.1'
+release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
